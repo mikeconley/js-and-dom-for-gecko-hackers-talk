a6de96d1c3ba71b7a2b66f2238ef68feb2b5d0d8
Mike Conley <mike.d.conley@gmail.com>
2015-09-24 14:04:41 -0400

    Fix up deploy script

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
 var rsync = require('gulp-rsync');
- 
+
 gulp.task('deploy', function() {
-  gulp.src('./*')
-    .pipe(rsync({
-      root: '.',
-      hostname: 'people.mozilla.org',
-      username: 'mconley2',
-      destination: '~/public_html/talks/js-and-dom-for-gecko-hackers-talk'
-    }));
+  return gulp.src(['{themes,extensions,core}/**/*.{js,css}', 'index.html'])
+             .pipe(rsync({
+               root: '.',
+               hostname: 'people.mozilla.org',
+               username: 'mconley2',
+               destination: '~/public_html/talks/js-and-dom-for-gecko-hackers-talk'
+             }));
 });
