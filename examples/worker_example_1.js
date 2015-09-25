c192f394ec97b3582c36281ebdcf57e499c80572
Mike Conley <mike.d.conley@gmail.com>
2015-09-25 14:13:42 -0400

    Flesh out Worker section

---
+++ examples/worker_example_1.js
@@ -0,0 +1,14 @@
+function log(msg) {
+  postMessage({
+    name: "Log",
+    msg: msg,
+  });
+}
+
+addEventListener("message", function(e) {
+  log("Worker received time to wait: " + e.data);
+  var ms = e.data * 1000;
+  var d = Date.now();
+  while (Date.now() - d < ms) {}
+  log("Worker is done waiting for: " + e.data + " seconds");
+});
